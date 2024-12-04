18e2c0055bc91075c14be8ee190fea138f8738c6
Anthony Brice <anthony.brice@cj.com>
2024-12-04 09:41:28 -0800

    d4p2 done

--- src/day4/part2.zig
+++ src/day4/part2.zig
@@ -1,32 +1,12 @@
 const std = @import("std");
 const util = @import("../main.zig");
 
-const directions = [_][2]i64{
-    .{ -1, -1 }, // up-left
-    .{ -1, 1 }, // up-right
-    .{ 1, -1 }, // down-left
-    .{ 1, 1 }, // down-right
-};
-
-const checks = [_][2]i64{
-    .{ 2, 0 }, // right
-    .{ 0, 2 }, // down
-};
-
-const Elem = struct {
-    char: u8,
-    up_left: bool,
-    up_right: bool,
-    down_left: bool,
-    down_right: bool,
-};
-
 pub fn main(allocator: std.mem.Allocator, path: []const u8) !void {
     const file_contents = try util.readFile(allocator, path);
     defer allocator.free(file_contents);
 
     var lines = std.mem.tokenizeSequence(u8, file_contents, "\n");
-    var map = std.AutoArrayHashMap([2]usize, Elem).init(allocator);
+    var map = std.AutoArrayHashMap([2]usize, u8).init(allocator);
     defer map.deinit();
 
     var row: usize = 0;
@@ -34,37 +14,50 @@ pub fn main(allocator: std.mem.Allocator, path: []const u8) !void {
     while (lines.next()) |line| {
         line_len = line.len;
         for (line, 0..) |char, col| {
-            try map.put(.{ row, col }, Elem{
-                .char = char,
-                .up_left = false,
-                .up_right = false,
-                .down_left = false,
-                .down_right = false,
-            });
+            try map.put(.{ row, col }, char);
         }
         row += 1;
     }
 
-    for (0..row - 1) |i| {
-        for (0..line_len - 1) |j| {
-            const elem: Elem = map.get(.{ i, j }).?;
-            if (elem.char == 'M') {
-                _ = isXmas(&map, i, j);
-            }
-        }
+    var iter = map.iterator();
+    var sum: u64 = 0;
+    while (iter.next()) |entry| {
+        const key = entry.key_ptr;
+        if (try isXmas(allocator, map, key[0], key[1])) sum += 1;
     }
+
+    std.debug.print("{d}\n", .{sum});
 }
 
-fn isXmas(map: *std.AutoArrayHashMap([2]usize, Elem), row: usize, col: usize) bool {
-    _ = map;
-    _ = row;
-    _ = col;
-    // for every unvisited direction, check if the next two characters are A and S
-    // if so, check right and down for an M
-    // If so, check cross-direction for an S
-    // if so, mark satisfied directions as visited and return true
+fn isXmas(
+    allocator: std.mem.Allocator,
+    map: std.AutoArrayHashMap([2]usize, u8),
+    row: usize,
+    col: usize,
+) !bool {
+    if (row == 0 or col == 0) return false;
+    const a = map.get(.{ row, col }) orelse return false;
+    if (a != 'A') return false;
 
-    // or grab every relevant cell, check, and if so, mark dirs as visited?
+    const above_left = map.get(.{ row - 1, col - 1 }) orelse return false;
+    const above_right = map.get(.{ row - 1, col + 1 }) orelse return false;
+    const below_left = map.get(.{ row + 1, col - 1 }) orelse return false;
+    const below_right = map.get(.{ row + 1, col + 1 }) orelse return false;
+
+    const arr = try std.fmt.allocPrint(
+        allocator,
+        "{c}{c}{c}{c}",
+        .{ above_left, above_right, below_left, below_right },
+    );
+    defer allocator.free(arr);
+
+    if (std.mem.eql(u8, arr, "MMSS") or
+        std.mem.eql(u8, arr, "SSMM") or
+        std.mem.eql(u8, arr, "MSMS") or
+        std.mem.eql(u8, arr, "SMSM"))
+    {
+        return true;
+    }
 
     return false;
 }
