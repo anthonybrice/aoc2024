c573467ec64658de0a602ce3bf0f87814086e3fe
Anthony Brice <anthony.brice@cj.com>
2024-12-09 00:18:18 -0800

    🤷‍♂️

--- src/day9/part2.zig
+++ src/day9/part2.zig
@@ -5,50 +5,16 @@ pub fn main(allocator: std.mem.Allocator, path: []const u8) !void {
     const file_contents = try util.readFile(allocator, path);
     defer allocator.free(file_contents);
 
-    var disk = try parseDisk(allocator, std.mem.trimRight(u8, file_contents, "\n"));
+    var disk = try Disk.initFromDenseMap(
+        allocator,
+        std.mem.trimRight(u8, file_contents, "\n"),
+    );
     defer {
         allocator.free(disk.blocks);
         disk.file_map.deinit();
     }
 
-    var max_file_id: u64 = 0;
-    for (disk.file_map.keys()) |file_id| {
-        if (file_id > max_file_id) max_file_id = file_id;
-    }
-
-    var file_id = max_file_id;
-    while (file_id >= 0) {
-        const file = disk.file_map.get(file_id).?;
-
-        // find first free segment in which file will fit
-        for (0..file.idx) |i| {
-            if (disk.blocks[i] == .free) {
-                var size: u64 = 0;
-                for (i..disk.blocks.len) |j| {
-                    if (disk.blocks[j] == .free) {
-                        size += 1;
-                    } else {
-                        break;
-                    }
-                }
-                if (size >= file.size) {
-                    for (i..i + file.size) |j| {
-                        disk.blocks[j] = DiskBlock{ .file = file_id };
-                    }
-                    for (file.idx..file.idx + file.size) |j| {
-                        disk.blocks[j] = DiskBlock{ .free = {} };
-                    }
-                    try disk.file_map.put(file_id, .{ .idx = i, .size = file.size });
-                    break;
-                }
-            }
-        }
-
-        if (file_id == 0) break;
-        file_id -= 1;
-    }
-
-    // printDisk(disk.blocks);
+    try disk.defrag();
 
     var sum: u64 = 0;
     for (0..disk.blocks.len) |i| {
@@ -61,54 +27,93 @@ pub fn main(allocator: std.mem.Allocator, path: []const u8) !void {
     std.debug.print("{d}\n", .{sum});
 }
 
-fn printDisk(disk: []DiskBlock) void {
-    for (0..disk.len) |i| {
-        const block = disk[i];
-        switch (block) {
-            .file => |file| std.debug.print("{d}", .{file}),
-            .free => std.debug.print(".", .{}),
-        }
-    }
-    std.debug.print("\n", .{});
-}
-
-const DiskBlock = union(enum) {
-    file: u64,
-    free,
-};
-
-const FileMapV = struct {
-    idx: usize,
-    size: u64,
-};
-
 const Disk = struct {
-    blocks: []DiskBlock,
-    file_map: std.AutoArrayHashMap(u64, FileMapV),
-};
+    blocks: []Block,
+    file_map: std.AutoArrayHashMap(u64, File),
 
-fn parseDisk(allocator: std.mem.Allocator, disk_map: []const u8) !Disk {
-    var blocks = std.ArrayList(DiskBlock).init(allocator);
-    var file_map = std.AutoArrayHashMap(u64, FileMapV).init(allocator);
-    var is_file = true;
-    var file_id: u64 = 0;
-    for (disk_map) |char| {
-        const size = try std.fmt.parseInt(u64, &[_]u8{char}, 10);
-        for (0..size) |i| {
-            var block: DiskBlock = undefined;
-            if (is_file) {
-                block = DiskBlock{ .file = file_id };
-            } else {
-                block = DiskBlock{ .free = {} };
-            }
-            try blocks.append(block);
-            if (is_file and i == 0) {
-                try file_map.put(file_id, .{ .idx = blocks.items.len - 1, .size = size });
-            }
+    const Block = union(enum) {
+        file: u64,
+        free,
+    };
+
+    const File = struct {
+        idx: usize,
+        size: u64,
+    };
+
+    fn defrag(self: *Disk) !void {
+        var max_file_id: u64 = 0;
+        for (self.file_map.keys()) |file_id| {
+            if (file_id > max_file_id) max_file_id = file_id;
         }
-        if (is_file) file_id += 1;
-        is_file = !is_file;
+
+        var file_id = max_file_id;
+        while (file_id >= 0) {
+            const file = self.file_map.get(file_id).?;
+
+            // find first free segment in which file will fit
+            for (0..file.idx) |i| {
+                if (self.blocks[i] == .free) {
+                    var size: u64 = 0;
+                    for (i..self.blocks.len) |j| {
+                        if (self.blocks[j] == .free) {
+                            size += 1;
+                        } else {
+                            break;
+                        }
+                    }
+                    if (size >= file.size) {
+                        for (i..i + file.size) |j| {
+                            self.blocks[j] = Disk.Block{ .file = file_id };
+                        }
+                        for (file.idx..file.idx + file.size) |j| {
+                            self.blocks[j] = Disk.Block{ .free = {} };
+                        }
+                        try self.file_map.put(file_id, .{ .idx = i, .size = file.size });
+                        break;
+                    }
+                }
+            }
+
+            if (file_id == 0) break;
+            file_id -= 1;
+        }
     }
 
-    return Disk{ .blocks = try blocks.toOwnedSlice(), .file_map = file_map };
-}
+    fn initFromDenseMap(allocator: std.mem.Allocator, disk_map: []const u8) !Disk {
+        var blocks = std.ArrayList(Disk.Block).init(allocator);
+        var file_map = std.AutoArrayHashMap(u64, File).init(allocator);
+        var is_file = true;
+        var file_id: u64 = 0;
+        for (disk_map) |char| {
+            const size = try std.fmt.parseInt(u64, &[_]u8{char}, 10);
+            for (0..size) |i| {
+                var block: Disk.Block = undefined;
+                if (is_file) {
+                    block = Disk.Block{ .file = file_id };
+                } else {
+                    block = Disk.Block{ .free = {} };
+                }
+                try blocks.append(block);
+                if (is_file and i == 0) {
+                    try file_map.put(file_id, .{ .idx = blocks.items.len - 1, .size = size });
+                }
+            }
+            if (is_file) file_id += 1;
+            is_file = !is_file;
+        }
+
+        return Disk{ .blocks = try blocks.toOwnedSlice(), .file_map = file_map };
+    }
+
+    fn printBlocks(self: Disk) void {
+        for (0..self.block.len) |i| {
+            const block = self.block[i];
+            switch (block) {
+                .file => |file| std.debug.print("{d}", .{file}),
+                .free => std.debug.print(".", .{}),
+            }
+        }
+        std.debug.print("\n", .{});
+    }
+};
